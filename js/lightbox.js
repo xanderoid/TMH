700c6b60c563fc3bf7984c8a919a5114eacef6cd
Xander Taylor <xanderoid@gmail.com>
2014-04-22 12:56:47 -0700

    added pseudo code in comments to lightbox.js to plan out coding

---
+++ js/lightbox.js
@@ -0,0 +1,14 @@
+//Problem: User when clicking on image goes to a dead end
+//Solution: Create an overlay with the large image (lightbox effect)
+
+
+//1. Capture click event on a link to an image
+	//1.1. Show the overlay.
+	//1.2 Update overlay with the image linked in the link
+	//1.3 Get child's alt attribute and set caption
+//2. Add overlay
+	//2.1 An image
+	//2.2 a caption
+	//optional: add navigation to overlay
+//3 When overlay is clicked
+	//3.1 Hide the overlay
