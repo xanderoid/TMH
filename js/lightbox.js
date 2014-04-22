4fe68db622e6483695fc5e3a48d7256db7c2106e
Xander Taylor <xanderoid@gmail.com>
2014-04-22 14:22:09 -0700

    improved lightbox layout, made img block level so it centers

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,14 +1,41 @@
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image (lightbox effect)
 
+//store overlay, image and caption in variable
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
 
-//1. Capture click event on a link to an image
-	//1.1. Show the overlay.
-	//1.2 Update overlay with the image linked in the link
-	//1.3 Get child's alt attribute and set caption
-//2. Add overlay
-	//2.1 An image
-	//2.2 a caption
+// add an image to overlay
+$overlay.append($image);
+// add a caption to overlay
+$overlay.append($caption);
+
+// Add overlay
+$("body").append($overlay);
+	
+
 	//optional: add navigation to overlay
+
+
+// Capture click event on a link to an image
+$("#imageGallery a").click(function(event){
+	event.preventDefault();
+	var imageLocation = $(this).attr("href");
+	//Update overlay with the image linked in the link
+	$image.attr("src", imageLocation);
+
+	//Show the overlay.
+	$overlay.show();
+	
+
+	//Get child's alt attribute and set caption	
+});
+
+
 //3 When overlay is clicked
+$overlay.click(function(){
 	//3.1 Hide the overlay
+	$overlay.hide();
+
+});
